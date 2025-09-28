d585c46e4880617b5a48e38d10fb31156cbcb66b
fairlaunchlabs <fairlaunchlabs@gmail.com>
2025-09-29 06:16:20 +0800

    flipflop-cli 1.3.12

--- cli/src/cli.ts
+++ cli/src/cli.ts
@@ -27,7 +27,7 @@ const program = new Command();
 program
   .name('flipflop')
   .description('A CLI tool for Flipflop token operations')
-  .version('1.3.11');
+  .version('1.3.12');
 
 // Add launch subcommand
 program.command('launch')
